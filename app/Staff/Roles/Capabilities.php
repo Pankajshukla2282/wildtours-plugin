351247530bfef3f8ccc15e48066368849a03a1a4
PWT <pannawildtouradmin@gmail.com>
2026-09-03 10:14:46 +0530

    Role capabilities

--- app/Staff/Roles/Capabilities.php
+++ app/Staff/Roles/Capabilities.php
@@ -18,5 +18,102 @@ final class Capabilities
     public const MANAGE_CONTENT = 'pwt_manage_travel_content';
     public const MANAGE_PRICING = 'pwt_manage_pricing';
 
-    public static function all(): array { return [self::ACCESS_DASHBOARD,self::MANAGE_STAFF,self::MANAGE_BOOKINGS,self::VIEW_CUSTOMERS,self::MANAGE_SAFARI,self::MANAGE_ACCOMMODATION,self::MANAGE_TRANSPORT,self::MANAGE_VENDORS,self::MANAGE_PAYMENTS,self::VIEW_REPORTS,self::MANAGE_CONTENT,self::MANAGE_PRICING]; }
+    public static function operationsManager(): array {
+        return [
+            self::ACCESS_DASHBOARD => true,
+            self::MANAGE_STAFF => true,
+            self::MANAGE_BOOKINGS => true,
+            self::VIEW_CUSTOMERS => true,
+            self::MANAGE_SAFARI => true,
+            self::MANAGE_ACCOMMODATION => true,
+            self::MANAGE_TRANSPORT => true,
+            self::MANAGE_VENDORS => true,
+            self::MANAGE_PAYMENTS => true,
+            self::VIEW_REPORTS => true,
+            self::MANAGE_CONTENT => true,
+            self::MANAGE_PRICING => true,
+            'edit_posts' => true,
+            'upload_files' => true,
+        ];
+    }
+
+    public static function bookingExecutive(): array {
+        return [
+            self::ACCESS_DASHBOARD => true,
+            self::MANAGE_BOOKINGS => true,
+            self::VIEW_CUSTOMERS => true,
+            self::VIEW_REPORTS => true,
+        ];
+    }
+
+    public static function safariCoordinator(): array {
+        return [
+            self::ACCESS_DASHBOARD => true,
+            self::MANAGE_SAFARI => true,
+            self::MANAGE_BOOKINGS => true,
+            self::VIEW_CUSTOMERS => true,
+        ];
+    }
+
+    public static function accommodationCoordinator(): array {
+        return [
+            self::ACCESS_DASHBOARD => true,
+            self::MANAGE_ACCOMMODATION => true,
+            self::MANAGE_BOOKINGS => true,
+            self::VIEW_CUSTOMERS => true,
+        ];
+    }
+
+    public static function transportCoordinator(): array {
+        return [
+            self::ACCESS_DASHBOARD => true,
+            self::MANAGE_TRANSPORT => true,
+            self::MANAGE_BOOKINGS => true,
+            self::VIEW_CUSTOMERS => true,
+        ];
+    }
+
+    public static function vendorManager(): array {
+        return [
+            self::ACCESS_DASHBOARD => true,
+            self::MANAGE_VENDORS => true,
+            self::MANAGE_ACCOMMODATION => true,
+            self::MANAGE_TRANSPORT => true,
+        ];
+    }
+
+    public static function accountsExecutive(): array {
+        return [
+            self::ACCESS_DASHBOARD => true,
+            self::MANAGE_PAYMENTS => true,
+            self::VIEW_REPORTS => true,
+            self::VIEW_CUSTOMERS => true,
+        ];
+    }
+
+    public static function contentExecutive(): array {
+        return [
+            self::ACCESS_DASHBOARD => true,
+            self::MANAGE_CONTENT => true,
+            'edit_posts' => true,
+            'upload_files' => true,
+        ];
+    }
+
+    public static function all(): array {
+        return [
+            self::ACCESS_DASHBOARD,
+            self::MANAGE_STAFF,
+            self::MANAGE_BOOKINGS,
+            self::VIEW_CUSTOMERS,
+            self::MANAGE_SAFARI,
+            self::MANAGE_ACCOMMODATION,
+            self::MANAGE_TRANSPORT,
+            self::MANAGE_VENDORS,
+            self::MANAGE_PAYMENTS,
+            self::VIEW_REPORTS,
+            self::MANAGE_CONTENT,
+            self::MANAGE_PRICING,
+        ];
+    }
 }
